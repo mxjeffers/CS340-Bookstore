68b784c7b39afb2302274fdd7406b90552a32018
mxjeffers <30639610+mxjeffers@users.noreply.github.com>
2020-11-01 10:32:21 +0300

    Created Author and BookAuthor Pages and queries.

--- public/addbooks.js
+++ public/addbooks.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', getBookData);
-
+const book_table = document.getElementById("book_table")
 
 function getBookData(){
     document.getElementById('search').addEventListener('click',(e)=>{
@@ -47,8 +47,45 @@ $('#database').on('click', (e)=>{
 
     //ajax data to server
     $.post("/addbook", payload)
-    .then(()=>{alert('Book Added')})
+    .then(()=>{alert('Book Added')
+                createbookTable()})
     .catch(()=>{alert("Book not added.")})
 
     
 })
+
+function createbookTable(){
+    $('#book_table').empty()
+    var headings = ["bookID","googleId","title","isbn","publisher","publishedDate",
+                    "description","pageCount","rating","price","quantityAvailable"];
+    
+    var tr = book_table.insertRow(-1);
+    for (var i=0; i < headings.length; i++){
+        var th = document.createElement('th')
+        th.innerHTML = headings[i];
+        tr.appendChild(th);
+    }
+    var booksdata
+    $.get("/getAllBooks",(bookdata)=>{
+        
+        maketable(bookdata)
+    })
+    
+}
+
+function maketable(bookdata){
+    $.each(bookdata,(key,val)=>{
+        var tr =book_table.insertRow(-1)
+
+        for(x in val){
+            td = document.createElement('td')
+            if(x == "description"){
+                td.innerHTML = "Description included."
+            } else{
+            td.innerHTML=(val[x])}
+            tr.appendChild(td)
+        }
+
+    })
+}
+createbookTable()
