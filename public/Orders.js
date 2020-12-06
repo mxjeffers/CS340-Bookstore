ded4d093ab0a8c9e8b9a2e45ae2f69edbf36bea2
JingChen <jingchenjc2019@gmail.com>
2020-12-06 23:23:45 +0800

    front-end changes.

--- public/Orders.js
+++ public/Orders.js
@@ -2,7 +2,7 @@
 const order_table = document.getElementById('orders_table')
 const details_table = document.getElementById('details_table')
 
-function createOrderTable(){
+function createOrderTable() {
     $('#orders_table').empty()
     var headings = ["orderId", "customerId", "customer", "orderDate"]
     var thead = document.createElement('thead')
@@ -14,19 +14,19 @@ function createOrderTable(){
         tr.appendChild(th)
         thead.appendChild(tr)
     }
-    $.get('/getOrders',(orderData)=>{
+    $.get('/getOrders', (orderData) => {
         order_table.appendChild(maketable(orderData))
-        $(document).ready(function(){
+        $(document).ready(function () {
             $('#orders_table').DataTable()
             order_tableditor()
-            $('#orders_table').on('draw.dt', () =>{
+            $('#orders_table').on('draw.dt', () => {
                 order_tableditor()
             })
         })
     })
 }
 
-function createDetailsTable(){
+function createDetailsTable() {
     $('#details_table').empty()
     var headings = ["orderid", "bookid", "Book Title", "Quantity Sold"]
     var thead = document.createElement('thead')
@@ -38,12 +38,12 @@ function createDetailsTable(){
         tr.appendChild(th)
         thead.appendChild(tr)
     }
-    $.get('/getOrderDetails',(detailsData)=>{
+    $.get('/getOrderDetails', (detailsData) => {
         details_table.appendChild(maketable(detailsData))
-        $(document).ready(function(){
+        $(document).ready(function () {
             $('#details_table').DataTable()
             details_tableditor()
-            $('#details_table').on('draw.dt', () =>{
+            $('#details_table').on('draw.dt', () => {
                 details_tableditor()
             })
         })
@@ -51,16 +51,17 @@ function createDetailsTable(){
 }
 
 
-function maketable(data){
-    tbody =  document.createElement('tbody')
+function maketable(data) {
+    tbody = document.createElement('tbody')
     $.each(data, (key, val) => {
         var tr = document.createElement('tr')
         for (x in val) {
             td = document.createElement('td')
-            if(val[x] == '' || val[x] == null){
+            if (val[x] == '' || val[x] == null) {
                 td.innerHTML = 'NULL'
-            }else{
-                td.innerHTML = (val[x])}
+            } else {
+                td.innerHTML = (val[x])
+            }
             tr.appendChild(td)
         }
         tbody.appendChild(tr)
@@ -68,126 +69,126 @@ function maketable(data){
     return tbody
 }
 
-function order_tableditor(){
+function order_tableditor() {
     $('#orders_table').Tabledit({
-        url:'/editOrders',
-        columns:{
-            identifier:[0,'orderId'],
-            editable:[[3,'orderDate']]
+        url: '/editOrders',
+        columns: {
+            identifier: [0, 'orderId'],
+            editable: [[3, 'orderDate']]
         },
         editButton: true,
-            deleteButton: true,
-    buttons: {
-      edit: {
-        class: 'btn btn-sm btn-primary',
-        html: 'EDIT',
-        action: 'edit'
-      },
-      delete: {
-        class: 'btn btn-sm btn-danger',
-        html: 'DELETE',
-        action: 'delete'
-      },
-      save: {
-        class: 'btn btn-sm btn-success',
-        html: 'Save'
-      },
-      restore: {
-        class: 'btn btn-sm btn-warning',
-        html: 'Restore',
-        action: 'restore'
-      },
-      confirm: {
-        class: 'btn btn-sm btn-warning',
-        html: 'Confirm'
-      }
-    },
-    onAjax: function (action, serialize) {
-        $('#orders_table').DataTable().destroy()
-        createOrderTable()
-      },
+        deleteButton: true,
+        buttons: {
+            edit: {
+                class: 'btn btn-sm btn-primary',
+                html: 'EDIT',
+                action: 'edit'
+            },
+            delete: {
+                class: 'btn btn-sm btn-danger',
+                html: 'DELETE',
+                action: 'delete'
+            },
+            save: {
+                class: 'btn btn-sm btn-success',
+                html: 'Save'
+            },
+            restore: {
+                class: 'btn btn-sm btn-warning',
+                html: 'Restore',
+                action: 'restore'
+            },
+            confirm: {
+                class: 'btn btn-sm btn-warning',
+                html: 'Confirm'
+            }
+        },
+        onAjax: function (action, serialize) {
+            $('#orders_table').DataTable().destroy()
+            createOrderTable()
+        },
     })
-    
+
 }
 
-function details_tableditor(){
+function details_tableditor() {
     $('#details_table').Tabledit({
-        url:'/editDetails',
-        columns:{
-            identifier:[0,'orderId'],
-            editable:[[1,'bookId']]
+        url: '/editDetails',
+        columns: {
+            identifier: [0, 'orderId'],
+            editable: [[1, 'bookId']]
         },
         editButton: true,
         autoFocus: false,
-    buttons: {
-      edit: {
-        class: 'btn btn-sm btn-danger',
-        html: 'DELETE',
-        action: 'remove'
-      },
-      
-      save: {
-        class: 'btn btn-sm btn-success',
-        html: 'Are you sure?'
-      },
-      
-    },
-    inputClass:'readonly form-control-plaintext',
-    onAjax: function (action, serialize) {
-        $('#details_table').DataTable().destroy()
-        createDetailsTable()
-      }
+        buttons: {
+            edit: {
+                class: 'btn btn-sm btn-danger',
+                html: 'DELETE',
+                action: 'remove'
+            },
 
+            save: {
+                class: 'btn btn-sm btn-success',
+                html: 'Are you sure?'
+            },
+
+        },
+        inputClass: 'readonly form-control-plaintext',
+        onAjax: function (action, serialize) {
+            $('#details_table').DataTable().destroy()
+            createDetailsTable()
+        }
     })
 }
 
-function selectbars(){
+function selectbars() {
     $('#customer_select').empty()
     $('#book_select').empty()
     $('#order_select').empty()
-    $.get('/getCustomers', customerdata =>{
-        $.each(customerdata,(key,val)=>{
-            $('#customer_select').append('<option value="'+val.customerId+'"> Id:'+
-            val.customerId+ " "+ val.firstName +" "+ val.lastName+'</option>')
+    $.get('/getCustomers', customerdata => {
+        $.each(customerdata, (key, val) => {
+            $('#customer_select').append('<option value="' + val.customerId + '"> Id:' +
+                val.customerId + " " + val.firstName + " " + val.lastName + '</option>')
         })
     })
-    $.get('/OrderedBooks', bookdata =>{
-        $.each(bookdata,(key,val)=>{
-            $('#book_select').append('<option value="'+val.BookId+'">'+val.title+'</option>')
+    $.get('/OrderedBooks', bookdata => {
+        $.each(bookdata, (key, val) => {
+            $('#book_select').append('<option value="' + val.BookId + '">' + val.title + '</option>')
         })
     })
-    $.get('/getOrders', orderdata =>{
-        $.each(orderdata, (key,val)=>{
-            $('#order_select').append('<option value="'+val.orderId+'">ID:'+
-            val.orderId+ " "+val.names+'</option>')
+    $.get('/getOrders', orderdata => {
+        $.each(orderdata, (key, val) => {
+            $('#order_select').append('<option value="' + val.orderId + '">ID:' +
+                val.orderId + " " + val.names + '</option>')
         })
     })
 }
 
-$('#addOrder').on('click', e =>{
+$('#addOrder').on('click', e => {
     e.preventDefault()
     var payload = {
-        customerId : $('#customer_select').val(),
-        orderDate : $('#order_date').val()}
-    $.post('/addOrder',payload,function(data,status,xhr){
+        customerId: $('#customer_select').val(),
+        orderDate: $('#order_date').val()
+    }
+    $.post('/addOrder', payload, function (data, status, xhr) {
         $('#orders_table').DataTable().destroy()
         createOrderTable()
         selectbars()
     })
-    
+
 })
 
-$('#addDetails').on('click', e =>{
+$('#addDetails').on('click', e => {
     e.preventDefault()
     var payload = {
-        orderId : $('#order_select').val(),
-        bookId : $('#book_select').val(),
-        quantitySold : $('#quantity_sold').val()
+        orderId: $('#order_select').val(),
+        bookId: $('#book_select').val(),
+        quantitySold: $('#quantity_sold').val()
     }
-    $.post('/addOrderDetails',payload,function(data,status,xhr){
+    $.post('/addOrderDetails', payload, function (data, status, xhr) {
         $('#details_table').DataTable().destroy()
         createDetailsTable()
-        
+
     })
 })
 
