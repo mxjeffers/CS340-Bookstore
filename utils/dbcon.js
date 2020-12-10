f9e8518162cddc492d6ae82bc4ddc7be47425959
JingChen <jingchenjc2019@gmail.com>
2020-12-10 22:13:33 +0800

    Increased connection limit.

--- utils/dbcon.js
+++ utils/dbcon.js
@@ -1,7 +1,7 @@
 //Need this file to keep the heroku running.
 var mysql = require('mysql')
 var pool = mysql.createPool({
-    connectionLimit: 5,
+    connectionLimit: 10,
     host: 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'v0497qsdglv31o3l',
     password: 'casjau1bq1c1019u',
