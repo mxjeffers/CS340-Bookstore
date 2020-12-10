0d6d50ed1ec9442f96824e7fa23808e0acc5744f
JingChen <jingchenjc2019@gmail.com>
2020-12-10 22:10:41 +0800

    dbcon.js restored.

---
+++ utils/dbcon.js
@@ -0,0 +1,12 @@
+//Need this file to keep the heroku running.
+var mysql = require('mysql')
+var pool = mysql.createPool({
+    connectionLimit: 5,
+    host: 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'v0497qsdglv31o3l',
+    password: 'casjau1bq1c1019u',
+    database: 'ngrvtz55fudf45bt',
+    dateStrings: true
+})
+
+module.exports.pool = pool
