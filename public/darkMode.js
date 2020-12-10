3a276e1eff32c89d0d4c6027bb8b281456b1a85d
JingChen <jingchenjc2019@gmail.com>
2020-12-10 22:06:30 +0800

    nav bar stick to the top

--- public/darkMode.js
+++ public/darkMode.js
@@ -7,6 +7,7 @@ const containers = document.querySelectorAll(".container")
 const cards = document.querySelectorAll(".card")
 const lists = document.querySelectorAll(".list-group-item")
 const tables = document.querySelectorAll(".table")
+
 // Listen for a click on the button
 btn.addEventListener("click", function () {
     // If the current URL contains "light-theme.css"
@@ -41,4 +42,22 @@ btn.addEventListener("click", function () {
             table.className = "table table-striped"
         })
     }
+
+
 });
+
+//styling for dark mode
+if (theme.getAttribute("href") == "/css/dark-theme.css") {
+    containers.forEach((cont, index) => {
+        cont.className = "container bg-dark text-white"
+    });
+    cards.forEach((card, index) => {
+        card.className = "card text-white bg-dark"
+    })
+    lists.forEach((list, index) => {
+        list.className = "list-group-item list-group-item-dark"
+    })
+    tables.forEach((table, index) => {
+        table.className = "table table-striped table-dark"
+    })
+}
